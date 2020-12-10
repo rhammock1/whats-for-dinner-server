ea3ff16e4b94a4219723449756f255b2327d2ebd
rhammock1 <rhamm1027@gmail.com>
2020-12-10 18:11:06 -0500

    added services

---
+++ src/restaurants/restaurants-service.js
@@ -0,0 +1,33 @@
+const restaurantsService = {
+  getAllFolders(knex) {
+    return knex.select('*').from('dinner_restaurants')
+  },
+  insertRestaurant(knex, newFolder) {
+    return knex
+      .insert(newRestaurant)
+      .into('dinner_restaurants')
+      .returning('*')
+      .then(rows => {
+        return rows[0]
+      })
+  },
+  getById(knex, id) {
+    return knex
+      .from('dinner_restaurants')
+      .select('*')
+      .where('id', id)
+      .first()
+  },
+  deleteRestaurant(knex, id) {
+    return knex('dinner_restaurants')
+      .where({ id })
+      .delete()
+  },
+  updateRestaurant(knex, id, newRestaurantId) {
+    return knex('dinner_restaurants')
+      .where({ id })
+      .update(newRestaurantId)
+  },
+};
+
+module.exports = restaurantsService;
