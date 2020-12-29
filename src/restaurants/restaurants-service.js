283aab8e342bfd8d66ec954ccbf01a7369d67a67
rhammock1 <rhamm1027@gmail.com>
2020-12-29 12:02:17 -0500

    Added get endpoint for user restaurants

--- src/restaurants/restaurants-service.js
+++ src/restaurants/restaurants-service.js
@@ -2,6 +2,12 @@ const restaurantsService = {
   getAllRestaurants(knex) {
     return knex.select('*').from('dinner_restaurants')
   },
+  getUsersRestaurants(knex, user_id) {
+    return knex
+      .select('*')
+      .from('dinner_restaurants')
+      .where({user_id})
+  },
   insertRestaurant(knex, newRestaurant) {
     return knex
       .insert(newRestaurant)
