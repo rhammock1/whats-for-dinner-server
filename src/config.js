1c710dc5b18e8983c1e80510254ab2c0429361ad
Robert Hammock <rhamm1027@gmail.com>
2021-03-01 13:14:08 -0500

    updates to adpt  new env

--- src/config.js
+++ src/config.js
@@ -1,12 +1,9 @@
 /* eslint-disable no-undef */
 'use strict';
-const dbURL = (process.env.NODE_ENV === 'development') 
-  ? 'postgres://dunder_mifflin@localhost/dinner'
-  : process.env.DATABASE_URL;
 
 module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: dbURL,
+  DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET || 's0m3th1ng-s3cr3t',
 };
