28857b747566ca006001bb040696cae735559677
Robert Hammock <rhamm1027@gmail.com>
2021-03-29 15:28:26 -0400

    troubleshooting

--- src/server.js
+++ src/server.js
@@ -5,12 +5,11 @@ const { PORT } = require('./config');
 const MODE = process.env.NODE_ENV;
 const knex = require('knex');
 
-const pgconfig = parse(proces.env.DATABASE_URL);
-pgconfig.ssl = { rejectUnauthorized: false }
 
 const db = knex({
   client: 'pg',
-  connection: (MODE === 'production') ? pgconfig : process.env.DATABASE_URL,
+  connection: process.env.DATABASE_URL,
+
 });
 
 app.set('db', db);
